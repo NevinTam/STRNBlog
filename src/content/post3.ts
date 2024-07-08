f17083a9f432d351e8bc851570b0614a21de115a
Nevin <nevin.tam@gmail.com>
2024-07-08 00:23:04 -0700

    New Post

---
+++ src/content/post3.ts
@@ -0,0 +1,16 @@
+import { Post } from '../types/post';
+
+const post: Post = {
+    slug: 'post3',
+    date: '2024-02-28T13:27:28Z',
+    title: 'This is a new post',
+    description: 'This is the description',
+    thumbnail: 'post3.png',
+    content: `
+        ## Test
+        # Hello
+        - Thanks
+    `,
+};
+
+export default post;
