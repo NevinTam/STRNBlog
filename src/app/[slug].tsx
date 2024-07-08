7d6b12116b8009d7d4ca07b6a5f163100b88008a
Nevin <nevin.tam@gmail.com>
2024-07-07 23:08:48 -0700

    Static Site

---
+++ src/app/[slug].tsx
@@ -0,0 +1,42 @@
+import { View, Text, ScrollView} from 'react-native'
+import React, { useState } from 'react'
+import { Stack, useLocalSearchParams } from 'expo-router'
+import { getPost, getAllPosts } from '../repository/postRepository';
+import Markdown from 'react-native-markdown-display';
+
+export async function generateStaticParams(): Promise<Record<string, string>[]> {
+    const posts = getAllPosts();
+    // Return an array of params to generate static HTML files for.
+    // Each entry in the array will be a new page.
+    return posts.map(post => ({ slug: post.slug }));
+  }
+
+
+const PostDetailsPage = () => {
+    const { slug } = useLocalSearchParams();
+const [post, setPost] = useState(getPost(slug));
+
+if(!post){
+    return <Text>Post not Found!</Text>
+}
+
+    return (
+        <ScrollView 
+          style = {{
+            flex: 1,
+            backgroundColor: 'white',
+        }}
+        contentContainerStyle ={{
+            maxWidth: 960,
+            width: '100%',
+            marginHorizontal: 'auto',
+            padding: 20,
+        }}
+    >
+            <Text style = {{ fontSize: 30, marginBottom: 20 }}>{post.title}</Text>
+            <Markdown>{post.content}</Markdown>
+        </ScrollView>
+    )
+}
+
+export default PostDetailsPage;
---
+++ src/content/post1.ts
@@ -0,0 +1,90 @@
+import { Post } from '../types/post';
+
+const post: Post = {
+    slug: 'post1',
+    date: '2024-02-28T13:27:28Z',
+    title: 'My first post',
+    description: 'This is My first post',
+    thumbnail: '',
+    content: `
+    # Ite in esse gratos clipeum creditur posse
+
+## Pertimui totum
+
+Lorem markdownum et prius protervis **hominum volant** videt comitemque quod
+[quamvis quisve enim](http://fama-actis.com/), precantibus manibus est crimen
+sagitta. Qui horruit triplex, postquam, Parrhasio modo falsamque congerit?
+Moveri ore aera semper, cornua aut Phrygum diem cum **non non**, non! Ignotos
+**rediit**. Coetum aura ictus et et Pithecusas animo iactura saltus?
+
+- Abiit quid Idaque pando diu admota condit
+- Et quis caelo
+- Alius hoc tempus virgo
+
+## Solent amantem iuvene manus fuit canis excussit
+
+Cum latus. Quam gente, qui undis Phoebi cognoscere habet, et insere frondibus
+*viribus* cuncta. Amens vitae causam subiectis aliquid notis rima inachus: non
+*incubuit omnibus mortale*, positaeque dedit quoque reparabile virgo:
+Thaumantidos. Herba habitataque deorum iuvenesque simul loquendo riget vertitur
+sicut, negetur inploraret falce, [exspatiata tutus
+dedecus](http://surgiscredar.com/per); per misit. **Infecit uti caudam** est
+furor verba: et [et](http://oculis-vere.com/) sub: induit defensamus: est?
+
+## Hostis subterque videret
+
+Crantor ab uno propiore, aut quid feritatis prima fatidicamque facta vellet
+dictas videt reposcunt ignis. Et arvis non undis astra, me Insula inque ducit.
+
+- Iussisque animaeque iunctis ad populis corpora laeva
+- Cornu vicina graviora non cum tectam cruor
+- Supplex aliquid quoniam
+- Petendum plus amatos est aliquid felici est
+- Occidit sub
+
+## Rabie gyrum magnum acclinia qui enses verba
+
+Ore loco pharetraque facis simul concitus Simois, victus numen umbrae genuumque
+*et* vincula Palladaque sol aris parari Troiae. Sanguine me tenebo terram, tibi
+novas? Terraeque causa propiore, infectaque pennas currebam Sicula promissa metu
+pro caeli litora.
+
+## Novissima bacis in quam copia
+
+**Notum pedem** puto est devertor hos Procne pavet. Seu nunc perdere multas,
+scopulum laniarat dextris? Ternisque inque, alumno verum [subitusque
+minari](http://suos.com/ante-servare) vel falcatus amore! Et disce moriens
+generumque obvertit ambarum totosque inque. Sanguine est arva genae visa et
+sequi habuisse fontes et sic pastor data, uni aurum dum.
+
+    ttl_subdirectory_software.typeface_nanometer_drm = e_market_nui;
+    if (1) {
+        controlPopRisc = dram_drive_upload(5, typefaceRam, serpFriendly);
+    } else {
+        readmeBaseSecondary += 54;
+        pc_whitelist_software = number;
+    }
+    if (ttl * -2 + trackballWhite(2)) {
+        lun.art += cutAdsl.ram(unicodeServer, token, cybersquatterBarcraft) + d;
+        social_mini_optical.cc_ruby += mashup(ray, copy(mashup));
+        adsl.cookie *= 3 * bandwidth(emoticon, 5);
+    } else {
+        display_only(raw_fragmentation_forum, software, publishing);
+        codeWais -= search_jquery;
+    }
+    var dataKerningVideo = intelligence_clip_dithering(
+            restoreShift.reciprocal_real(fileYahoo, domainAntivirus,
+            xmlTooltipCdfs * ad));
+    var mbr = 229733;
+
+Cyclopum moriens est iuves, iamque viriles exsiluere saepe, ire. Flamina adfuit,
+init, instructamque castris traherent placuisse oneratos pinnis errat corpus
+multiplicique terrae forma est, si. Sumit Othrys sub maius litoris medicamine
+generis remollescit, Hippomenen dixit remansit causa, frigus non vario!
+Pendentia ut callidus quin pudibundaque **ponar** feras matrum commonuit Sparte
+linguaque sinu submoverat erubuit teste relinquit ad. Vetus fulmina illa
+sorores.
+    `,
+};
+
+export default post;
