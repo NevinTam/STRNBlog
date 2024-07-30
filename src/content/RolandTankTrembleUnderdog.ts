e8a799708f1970ba5691e2d8ab9be7fc9cda169f
Nevin <nevin.tam@gmail.com>
2024-07-29 18:28:36 -0700

    Removed 'Findlay' from articles

--- src/content/RolandTankTrembleUnderdog.ts
+++ src/content/RolandTankTrembleUnderdog.ts
@@ -3,8 +3,8 @@ import { Post } from '../types/post';
 const post: Post = {
     slug: 'RolandTankTrembleUnderdog', //file name
     date: '2024-07-29T13:27:28Z',
-    title: 'Findlay’s Underdogs: How Roland "Tank" Tremble Approaches his Time at Findlay',
-    description: 'Findlay has served as an important transition for many football players looking to make their name in D2 football. Part of this is due to Findlay’s recruiting program, which scouts many local players as potential talent for their program. One recent result of this recruitment program was Roland “Tank” Tremble, who committed to the program just this past February. ',
+    title: 'Football Underdogs: How Roland "Tank" Tremble Approaches his Football Journey',
+    description: 'Roland "Tank" Tremble profile!',
     thumbnail: 'https://substackcdn.com/image/fetch/w_1456,c_limit,f_webp,q_auto:good,fl_progressive:steep/https%3A%2F%2Fsubstack-post-media.s3.amazonaws.com%2Fpublic%2Fimages%2Ff3b0be53-e370-431d-a339-ac99308cfa57_1152x2048.jpeg',
     content: `
     
@@ -12,11 +12,9 @@ const post: Post = {
     SeahawksToday Lead Writers: Shrihun Sankepally and Nevin Tamilselvan
 ‎ 
 
-Findlay has served as an important transition for many football players looking to make their name in D2 football. Part of this is due to Findlay’s recruiting program, which scouts many local players as potential talent for their program. One recent result of this recruitment program was Roland “Tank” Tremble, who committed to the program just this past February. 
-‎ 
 ‎ 
 # **Roland "Tank" Tremble**... 
-is originally from Savannah, Georgia. He moved to Indiana during grade school and was eventually scouted by many colleges as he continued to play high school football. He chose Findlay due to the familial aspect that reminded him of his own family in Indiana, who have continued to push him all throughout his life.
+is originally from Savannah, Georgia. He moved to Indiana during grade school and was eventually scouted by many colleges as he continued to play high school football. He chose his college due to the familial aspect that reminded him of his own family in Indiana, who have continued to push him all throughout his life.
 
 > “It’s like a real family atmosphere. As soon as you get in, there’s no stranger. ‘Hey what’s your name?"‘ and you’re right into the family just like that.”
 
@@ -24,11 +22,11 @@ Along with his family, Roland credits his two high school defensive line coaches
 
 > “They helped me train. They also mentally got me prepared, they let me know that college is a different set of work”
 
-After joining the Findlay program just a few months ago, Roland remarks that another player that has really helped get him adjusted right from the first day was Senior OL Gabe Hemmelgarn. 
+After joining the D2 program just a few months ago, Roland remarks that another player that has really helped get him adjusted right from the first day was Senior OL Gabe Hemmelgarn. 
 
 > “First day [he] took me in. Started lifting with him … he’s been pushing me since the first day, so I’d really say he took me under his wing … and all the other guys (Farmer, etc) as well.”
 
-“Tank” credits the family atmosphere of Findlay for being so receptive to new players like himself, allowing everyone to get along with each other.
+“Tank” credits the family atmosphere of the program for being so receptive to new players like himself, allowing everyone to get along with each other.
 
 > “It’s like a real family atmosphere, like as soon as you get in there’s no stranger. ‘Ay what’s your name’, and then you’re right in the family just like that.”
 
@@ -38,7 +36,7 @@ Asked about Head Coach Kory Allen, despite only being a freshman, Roland remembe
 
 > “First workout, I got yelled at for not finishing through the line. After practice, ‘Ay you got to finish through the line and pick up your shoulders as you get going’. But at the end of practice, it’s family.
 
-After playing on the offensive line in highschool but then switching to defense and being recruited to Findlay on defense, Tank explains his reasoning for fully switching to defense despite OL longer:
+After playing on the offensive line in highschool but then switching to defense and being recruited to the program on defense, Tank explains his reasoning for fully switching to defense despite OL longer:
 
 # **”I like to hit people”**
 
@@ -54,11 +52,11 @@ The part of his game that is the most interesting to Tremble may surprise most d
 
 ‎ 
 # **Family**
-Even with the strong familial presence at Findlay, Tank still has to credit his own family for getting him to where he is today.
+Even with the strong familial presence at the school, Tank still has to credit his own family for getting him to where he is today.
 
 > “My dad started me out at football at a young age. As soon as I was able to run, I was playing football. My dad definitely kept me in it. My mom, of course, is always pushing me. My little brother played and I love seeing him grow up and play the game. And my friends who always are telling me ‘get up, don’t be lazy’”.
 
-Asked about if there’s anything he wished had gone differently for him in his young college and high school career, Tank stayed true to Findlay.
+Asked about if there’s anything he wished had gone differently for him in his young college and high school career, Tank stayed true to the school.
 
 > “I feel like if I didn’t get injured in my junior year, I might be in a different spot. But I am happy it came to this, because maybe I wasn’t supposed to be on a big campus, maybe I needed to start here. You get here and you put in the work. You either succeed or you don’t.”
 
@@ -70,7 +68,7 @@ Tank’s **biggest advice** he would give to all football players:
 ‎ 
 “Tank” Tremble on where his nickname “Tank” came from:
 
-> “A lot of coaches when I was getting recruited all over the place were calling me ‘Big Dawg Tank‘ and Coach Cole before he left Findlay called me Tank and then all of my highschool coaches started calling me it and it caught on”.
+> “A lot of coaches when I was getting recruited all over the place were calling me ‘Big Dawg Tank‘ and Coach Cole before he left the program called me Tank and then all of my highschool coaches started calling me it and it caught on”.
 
 ![](https://images2.minutemediacdn.com/image/upload/c_crop,w_3403,h_1914,x_590,y_64/c_fill,w_1200,ar_4:3,f_auto,q_auto,g_auto/images/voltaxMediaLibrary/mmsport/all_seahawks/01hzp0p5abhh9dvwczc9.jpg)
 
@@ -78,7 +76,7 @@ Even though Tank hasn’t really connected to **Seahawks OL Mike Jerrell**, his 
 
 > “I hear about him all the time and just the work he put in is insane. He keeps me honest and '‘you can do it too if you put in the amount of work he put in’. He inspired me for sure”.
 
-Despite only being at Findlay for a few months, Tank is **determined** to improve in whatever way possible.  
+Despite only being at the program for a few months, Tank is **determined** to improve in whatever way possible.  
 
 > “Growing for sure. Whether that is football or my degree. At the end of the day, football doesn’t last forever. I’m focused on getting my degree, and I just happen to get to play football is how I am thinking about it”.
 
