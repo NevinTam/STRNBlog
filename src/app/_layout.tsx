d16858ccf99b91b6b8226b5a57a282301aea8a05
Nevin <nevin.tam@gmail.com>
2024-08-23 19:50:11 -0700

    twitter embed check 12

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -9,7 +9,7 @@ import { initializeGA, logPageView } from '../analytics/ga4';
 import Head from 'expo-router/head';
 
 const fallbackMeta = {
-  title: 'SeahawksToday7',
+  title: 'SeahawksToday9',
   description: 'SeahawksToday - Your source for the latest Seahawks news.',
   thumbnail: 'https://substackcdn.com/image/fetch/w_176,h_176,c_fill,f_webp,q_auto:good,fl_progressive:steep,g_auto/https%3A%2F%2Fbucketeer-e05bbc84-baa3-437e-9518-adb32be77984.s3.amazonaws.com%2Fpublic%2Fimages%2F60c80449-366b-47dc-a711-17219ba57e61_463x427.png'
 };
@@ -149,11 +149,8 @@ export default function RootLayout() {
     const fetchPost = async () => {
       const posts = await getAllPosts();
       if (slug && typeof slug === 'string') {
-        // Ensure slug is a string and use it to find the specific post
         const post = posts.find(p => p.slug === slug || (slug.startsWith(p.slug) && slug.length > p.slug.length));
         setCurrentPost(post || null);
-      } else {
-        setCurrentPost(null);
       }
     };
     fetchPost();
@@ -191,20 +188,22 @@ export default function RootLayout() {
   const isHomePage = !slug || slug === '/' || slug === 'https://www.seahawks-today.com/' || slug === 'https://seahawks-today.com/';
   const isSpecialPage = slug === 'aboutPage' || slug === 'contactUs' || slug === 'twitter';
 
-  const meta = currentPost && !isHomePage && !isSpecialPage ? {
-    title: currentPost.title,
-    description: currentPost.description,
-    thumbnail: currentPost.thumbnail
-  } : fallbackMeta;
-
+  const meta = currentPost
+    ? {
+        title: currentPost.title,
+        description: currentPost.description,
+        thumbnail: currentPost.thumbnail,
+      }
+    : fallbackMeta;
+    
   return (
     <View style={styles.container}>
-      <Head>
+   <Head>
         <title>{meta.title}</title>
         <meta name="description" content={meta.description} />
-        <meta name="og:title" content={meta.title} />
-        <meta name="og:description" content={meta.description} />
-        <meta name="og:image" content={meta.thumbnail} />
+        <meta property="og:title" content={meta.title} />
+        <meta property="og:description" content={meta.description} />
+        <meta property="og:image" content={meta.thumbnail} />
         <meta name="twitter:title" content={meta.title} />
         <meta name="twitter:description" content={meta.description} />
         <meta name="twitter:image" content={meta.thumbnail} />
