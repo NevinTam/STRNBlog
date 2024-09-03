0ec2700ebef3632c32c3b79548c3f8a9b5b97d9e
OverlordsIII <shrihunreddy@outlook.com>
2024-09-02 20:05:35 -0700

    should work now

---
+++ src/content/oldSeattleSeahawksDraftRecap.ts
@@ -0,0 +1,14 @@
+import { Post } from '../types/post';
+
+const post: Post = {
+    slug: '', //file name
+    date: '2024-02-28T13:27:28Z',
+    title: '',
+    description: '',
+    thumbnail: '',
+    content: `
+    
+    `,
+};
+//how to add post: https://www.youtube.com/watch?v=ewW6_baBXko
+export default post;
