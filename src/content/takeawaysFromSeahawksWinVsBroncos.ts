1f651baf30a156fce6669e1955a106017563a51a
Shrihun Sankepally <69648849+OverlordsIII@users.noreply.github.com>
2024-09-08 21:02:10 -0700

    Create takeawaysFromSeahawksWinVsBroncos.ts

---
+++ src/content/takeawaysFromSeahawksWinVsBroncos.ts
@@ -0,0 +1,43 @@
+import { Post } from '../types/post';
+
+const post: Post = {
+    slug: 'takeawaysFromSeahawksWinVsBroncos', //file name
+    date: '2024-09-08T12:38:08.592455800',
+    title: 'Takeaways From Seahawks Opener Win Over Broncos',
+    description: 'Read below for takeaways from the Seahawks 26-20 win over the Broncos in the season opener!',
+    thumbnail: 'https://substackcdn.com/image/fetch/f_auto,q_auto:good,fl_progressive:steep/https%3A%2F%2Fsubstack-post-media.s3.amazonaws.com%2Fpublic%2Fimages%2F1925c7bc-fe4b-4230-bf6f-85ec69103dbc_755x500.jpeg',
+    content: `
+    **Geno Smith**
+    Seahawks QB Geno Smith was immediately put in a tough situation, getting sacked on the first play of the game and and overall playing for his life behind a downright atrocious offensive line performance for the first few drives. Smith managed to make some magic happen later in the first half after a huge 34-yard rushing TD where he outran Broncos S PJ Locke. In the second half, there was less of a load on Smith’s shoulders as the offense focused more on running the ball, allowing Geno to manage the game and take opportunities when given, such as several plays to Lockett and a early 4th quarter touchdown to Zach Charbonnet on a busted coverage.
+
+    ![](https://substackcdn.com/image/fetch/w_1456,c_limit,f_webp,q_auto:good,fl_progressive:steep/https%3A%2F%2Fsubstack-post-media.s3.amazonaws.com%2Fpublic%2Fimages%2F13379847-21b5-4b19-af3f-d86e12522bd4_750x500.jpeg)
+    
+    **Kenneth Walker and the Rushing Attack**
+    The Seahawks likely do not win this game without Walker. He was slow in the first half, recording under 40 yards on poor efficiency. However, OC Ryan Grubb’s gameplan shifted in the second half, leading to more outside runs for Walker that allowed him to get chunk yards and slowly wear down the Broncos defense. This helped (as stated before) to open up the offense and truly expose holes and busted coverages in the Broncos defense.
+
+    **Tyler Lockett**
+    Tyler Lockett was once again the most reliable receiver on the field, who had great chemistry with Geno on several throws, including a back shoulder 3rd down catch and a one-handed catch to ice the game. He ended up as the leading Seahawks reciever on the day, which was great to see after many speculated that this could be Lockett’s last or second last year with the Seahawks.
+
+    ![](https://substackcdn.com/image/fetch/w_1456,c_limit,f_webp,q_auto:good,fl_progressive:steep/https%3A%2F%2Fsubstack-post-media.s3.amazonaws.com%2Fpublic%2Fimages%2F0d5f407b-f7d5-4344-b97f-aff5de70a4e3_750x500.jpeg)
+    
+    **Defensive Line**
+    Mike Macdonald truly showed what his line could do today. They were bringing pressure from all different fronts and even having players like Jarran Reed or Leonard Williams drop into coverage at time to mask the Seahawk’s blitzes. They were stout against the run, limiting the Broncos to just under 100 rushing yards (35 of which came from Bo Nix scrambles late into the game). Byron Murphy and Leonard Williams were huge contributors to this, constantly eating blocks on the defensive line. OLBs Boye Mafe and Derrick Hall also had good contain on Bo Nix on several plays, leading to sacks for the both of them after pushing him out of bounds.
+
+    ![](https://substackcdn.com/image/fetch/w_1456,c_limit,f_webp,q_auto:good,fl_progressive:steep/https%3A%2F%2Fsubstack-post-media.s3.amazonaws.com%2Fpublic%2Fimages%2F4905e5f6-352c-4461-8916-00f8992eb585_750x500.jpeg)
+
+    **Linebackers**
+    Tyrell Dodson had a simply incredible game today. He was everywhere, from making huge tackles over the middle of the field and in the screen game. His coverage was great and he easily could have had an interception if he didn’t drop it. Jerome Baker also had a great game, especially when making stops in the run game. He also recovered a fumble and was sound in coverage. Both linebackers were often seen flying around the field to wherever the ball carrier had the ball, making their presence felt.
+
+    ![](https://substackcdn.com/image/fetch/w_1456,c_limit,f_webp,q_auto:good,fl_progressive:steep/https%3A%2F%2Fsubstack-post-media.s3.amazonaws.com%2Fpublic%2Fimages%2Feb597b25-5089-4901-b6ca-772e1b3844b1_625x500.jpeg)
+
+    **Secondary**
+    The secondary was perhaps the most dominant unit out of all of them today. Bo Nix completed only had a 3.3 YPA with 135 yards total. He consistently looked lost when pushing the ball downfield and had numerous dropped interceptions that only underestimated the secondary dominance. Julian Love and Tariq Woolen both recorded an interception. Woolen especially had a good game against Sutton, limiting him to under 40 yards. We also saw Woolen sometimes travel with Sutton, which Macdonald said he would likely be doing this season. Witherspoon and Tre Brown also had good games, with Witherspoon having several near-interceptions forced. K’von Wallace also forced a huge fumble that helped change the momentum in favor of the Seahawks.
+
+    ![](https://substackcdn.com/image/fetch/w_1456,c_limit,f_webp,q_auto:good,fl_progressive:steep/https%3A%2F%2Fsubstack-post-media.s3.amazonaws.com%2Fpublic%2Fimages%2Fae474dbc-8324-451b-b83a-972b76befda3_750x500.jpeg)
+
+    **Special Teams**
+    While it was good for the most part, the punt return game by Dee Williams had several huge problems in the first half. Williams during one return tried to field the punt but muffed it. He then tried to pick it up again and run but by then he had lost control and the Broncos recovered, eventually hitting a field goal. On the next return, Williams let the ball go and the Broncos were able to down the ball at the one-yard line, leading to a second Broncos safety. While this was a rough game for him, it was his first game as an NFL player and he will get many opportunities to continue his return skills alongside Laviska Shenault, who was the second returner on kickoffs.
+    `,
+};
+//how to add post: https://www.youtube.com/watch?v=ewW6_baBXko
+export default post;
