a5d4d32b72739136b39ff9d5b63717a0e0a1fdaa
Shrihun Sankepally <69648849+OverlordsIII@users.noreply.github.com>
2024-09-29 18:14:08 -0700

    Create takeawaysFromSeahawksVsDolphins.ts

---
+++ src/content/takeawaysFromSeahawksVsDolphins.ts
@@ -0,0 +1,42 @@
+import { Post } from '../types/post';
+
+const post: Post = {
+    slug: 'takeawaysFromSeahawksVsDolphins', //file name
+    date: '2024-09-29T12:38:08.602300300',
+    title: 'Takeaways From Seahawks Win Over Dolphins',
+    description: 'The Seahawks improve to 3-0 taking down the Dolphins at home. Read Below for our takeaways from the game!',
+    thumbnail: 'https://substackcdn.com/image/fetch/w_1456,c_limit,f_webp,q_auto:good,fl_progressive:steep/https%3A%2F%2Fsubstack-post-media.s3.amazonaws.com%2Fpublic%2Fimages%2Fcafdeca3-d04f-4132-be82-d02136ee2538_760x427.jpeg',
+    content: `
+    Seahawks Today Lead Writers: Shrihun Sankepally and Nevin Tamilselvan
+
+**Geno Smith continues his play, for the most part**
+
+Geno Smith had another mostly good game against the Dolphins, posting 289 yards on over 75% completion percentage. He had 2 unfortunate interceptions, however. On the first interception he was under heavy pressure and threw the ball slightly high for Zach Charbonnet, who tipped the ball up in the air for an interception. The second pick was off another tipped ball, this time by Calais Campbell. Look for Geno to attempt and keep up this higher level of play next week against the Lions on MNF, which should be a huge test for this up and coming Seahawks team.
+
+![](https://substackcdn.com/image/fetch/w_1456,c_limit,f_webp,q_auto:good,fl_progressive:steep/https%3A%2F%2Fsubstack-post-media.s3.amazonaws.com%2Fpublic%2Fimages%2F3409d32a-fb08-461c-a62f-16e5f0c745ae_1000x600.jpeg)
+
+**Run game gets going**
+
+While the run game was suboptimal at parts in this game, towards the latter half the Seahawks were able to utilize Zach Charbonnet much more effectively this game, allowing him to be a workhorse that supported Geno. This led to them finally putting away the game in the 4th quarter. However, there were several key blocks missed in the run game from players like Anthony Bradford, who recorded another 2 penalties and is having his position as a starter seriously challenged. Going into next week’s game, the Seahawks are likely to have Kenneth Walker back, but will need everything they can get from Bradford and possibly Haynes (if there is a rotation) while facing a stout Lions defensive line.
+
+![](https://substackcdn.com/image/fetch/w_1456,c_limit,f_webp,q_auto:good,fl_progressive:steep/https%3A%2F%2Fsubstack-post-media.s3.amazonaws.com%2Fpublic%2Fimages%2Fbb15c1de-1114-4018-a4ff-d39e71628149_1280x720.jpeg)
+
+**Sub-optimal Guard Play**
+
+As mentioned previously, the guard play in this game was not great. Laken Tomlinson, while playing fine in run-blocking, has not lived up in pass protection, leading QB Geno Smith to being pressured on over 40% of his dropbacks. This against a team without one of their best pass rushers in Bradley Chubb. Bradford, on the other hand, also missed several key blocks in the pass and run game, making him one of the most negative players on the entire Seahawks starting lineups. While there is a growing reason for why many want Bradford to be benched, OC Ryan Grubb mentioned that Bradford won his spot against rookie Christian Haynes, citing that Haynes was not ready yet. However, HC Mike Macdonald said that Haynes would see some limited action, like he did in the Patriots game in week 2. Look for a potential rotation as they head into MNF.
+
+![](https://substackcdn.com/image/fetch/f_auto,q_auto:good,fl_progressive:steep/https%3A%2F%2Fsubstack-post-media.s3.amazonaws.com%2Fpublic%2Fimages%2F205604b4-e066-4797-8840-05db65925416_3748x2499.jpeg)
+
+**Pass Rush Overcomes Injuries, Maintains Top-Tier Play**
+
+Having lost 2 of their main interior pass rushers, the Seahawks were able to employ the “next man up” philosophy and record 6 sacks while constantly pressuring QBs Skylar Thompson and Tim Boyle. Derrick Hall had his best game yet, recording two sacks and tying Boye Mafe for the team lead in sacks. Hall has had a huge step up in these past two weeks after filling in for Uchenna Nwosu, who has been sidelined with a preseason injury. If the Seahawks want to make a statement next week on MNF, they will need to continue to fill in for Leonard Williams and Byron Murphy with players like Myles Adams, Mike Morris, Jarran Reed, and more.
+
+![](https://substackcdn.com/image/fetch/f_auto,q_auto:good,fl_progressive:steep/https%3A%2F%2Fsubstack-post-media.s3.amazonaws.com%2Fpublic%2Fimages%2F5ddc267e-148e-46b8-b3e5-9044a230575f_1280x720.jpeg)
+
+**Secondary Shuts Down Tough WR Core**
+
+Another great game for the secondary, who were able to mostly lock up Dolphins WRs Tyreek Hill and Jaylen Waddle for miniscule yardage. Tariq Woolen continued his All-Pro level season, pitching another coverage shutout. Tre Brown allowed one big reception but besides that played very well during the game. Witherspoon, however, had one of his worst games as a pro, recording two penalties and allowing 6 receptions against the Dolphins. While most of these were shorter yardage, Witherspoon and the rest of the secondary will have their hands full next week on MNF, as they play one of the best slot receivers in the league with Amon Ra St. Brown.
+    `,
+};
+//how to add post: https://www.youtube.com/watch?v=ewW6_baBXko
+export default post;
