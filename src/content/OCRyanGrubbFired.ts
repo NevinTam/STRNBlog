816ff0adea32cb03bc5f15ede41d1ee11f9c9dd7
Nevin <nevin.tam@gmail.com>
2025-01-06 18:30:40 -0800

    RyanGrubbArticleSpacing

--- src/content/OCRyanGrubbFired.ts
+++ src/content/OCRyanGrubbFired.ts
@@ -10,6 +10,7 @@ const post: Post = {
     Seahawks Today Lead Writers: Shrihun Sankepally and Nevin Tamilselvan
 
 **What Worked**
+
 Wide Receiver Jaxon Smith-Njigba breaking out was one of Ryan Grubb's best moments of his Seahawks OC tenure. Under former Seahawks OC Shane Waldron, Jaxon Smith-Njigba struggled mightily in getting involved in the offense. For a few weeks this season, the trend continued but halfway through the season, Smith-Njigba had an elite stretch, breaking and tying multiple franchise records. For reference, JSN tied the Seahawks single-season receptions record with 100, staying even with Tyler Lockett who has held the record since 2020. JSN also had the most consecutive games for a Seahawks WR to post 70 or more receiving yards. 
 
 DK Metcalf also seemed to have an elite start to the year. Metcalf was in the top-5 for wide recevier receiving yards earlier this season, before his injury that held him out for nearly a month after the game against the Atlanta Falcons. Metcalf tied his receptions total from last year, totaling 66 on the season. Geno Smith broke many franchise records this year, many set by himself in 2022. He became the 20th quarterback with 70+% in a single season. He threw for 4320 yards, the most by any quarterback in Seahawks history.
@@ -18,6 +19,7 @@ Another player who seemed to have "broken out" with Ryan Grubb at the helm of th
 ![](https://substackcdn.com/image/fetch/w_1456,c_limit,f_webp,q_auto:good,fl_progressive:steep/https%3A%2F%2Fsubstack-post-media.s3.amazonaws.com%2Fpublic%2Fimages%2Fdb8816eb-fdd4-459d-bedd-577d7cf4c545_900x509.jpeg)
 
 **What Didn't Work**
+
 Ryan Grubb came from a pass-heavy offense at the University of Washington with an elite offensive line and a first-round-pick in Michael Penix Jr. On the Seahawks, it felt like he often forgot that he did not have an elite offensive line anymore. Many times, the Seahawks would be left needing a few yards and Grubb would have every route run deep with no checkdown option for Geno Smith, leading to a change of possession when the team only needed a few yards. Other times in 3rd and short situations, Grubb would run the ball outside with these elaborate run-game matchups, having his tight ends block the opposing defense's best EDGE player and consequently seeing the running back get tackled behind the line of scrimmage. Grubb was also unable to use Noah Fant effectively, who signed an extension in the offseason and only had 1 touchdown on the entire season.
 
 Kenneth Walker is an All-Pro caliber player. Under Ryan Grubb, he had career low in yards, yards per carry, and touchdowns. The current offensive line was extremely inconsistent; however, Ryan Grubb often went into halftime with his star running back only having 3 or 4 carries, representative of Grubb's willingness to immediately abandon the run whenever down on the scoreboard. 
